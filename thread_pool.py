d95d3a4199c76e088c8647533ddeea034f3341f2
Grady Wong <wgqimut@gmail.com>
2015-07-05 00:20:36 +0800

    make the thread can return something.

--- thread_pool.py
+++ thread_pool.py
@@ -1,67 +1,103 @@
 #! /usr/bin/env python
 # _*_ coding:utf-8 _*_
 import time
+import sys
 
 __author__ = 'ilbsmart'
 
 import Queue
 import threading
+import traceback
 
-class MyThreadPool(object):
-    def __init__(self, thread_num, work_queue_num):
+
+def _handle_thread_exc(task, exc_info):
+    traceback.print_exception(*exc_info)
+
+
+class Task(object):
+    def __init__(self, thread_func, args=None, kwargs=None, callback=None):
+        self.thread_func = thread_func
+        self.args = args or []
+        self.kwargs = kwargs or {}
+        self.request_id = id(self)
+        self.callback = callback
+        self.exception = False
+        self.exc_callback = _handle_thread_exc
+
+
+class NoThreadsAvailable(Exception):
+    pass
+
+
+class ThreadPool(object):
+    def __init__(self, thread_num, work_queue_num, result_queue_num):
         self.th_nu = thread_num
         self.work_queue = Queue.Queue(work_queue_num)
+        self.result_queue = Queue.Queue(result_queue_num)
         self.threads = []
-        self._create_threads()
-        for i in range(100):
-            self.add_task(just_print, (i, ))
+        self._init_thread_pool()
 
-    def _create_threads(self):
-        for i in range(self.th_nu):
-            self.threads.append(WorkThread(self.work_queue))
+    def _init_thread_pool(self):
+        for i in range(1, self.th_nu):
+            self.threads.append(WorkThread(self.work_queue, self.result_queue))
 
-    def add_task(self, thread_func, args):
-        self.work_queue.put((thread_func, tuple(args)))
+    def add_task(self, thread_func, args=None, kwargs=None, callback=None):
+        self.work_queue.put(Task(thread_func, args, kwargs, callback))
 
+    def wait(self):
+        while True:
+            if not self.threads:
+                raise (NoThreadsAvailable, "no threads available")
+            try:
+                task, result = self.result_queue.get()
+                if task.exception:
+                    task.exc_callback(task, result)
+                elif task.callback:
+                    task.callback(task, result)
+            except Queue.Empty():
+                break
 
-    def destroy(self):
+    def fini_thread_pool(self):
         for i in self.threads:
             if i.isAlive():
                 i.join()
 
+
 class WorkThread(threading.Thread):
-    def __init__(self, work_queue):
+    def __init__(self, work_queue, result_queue):
         super(WorkThread, self).__init__()
-        self.work_queue = work_queue
+        self._work_queue = work_queue
+        self._result_queue = result_queue
         self.start()
 
     def run(self):
         while True:
             try:
-                (thread_func, args) = self.work_queue.get()
-                thread_func(args)
-                self.work_queue.task_done()
-            except Exception:
-                break
+                task = self._work_queue.get()
+                result = task.thread_func(task.args, task.kwargs, task.callback)
+                self._result_queue.put((task, result))
+                self._work_queue.task_done()
+            except:
+                task.exception = True
+                self._result_queue.put((task, sys.exc_info()))
 
-def just_print(args):
-    print "Helo"
-    print "thread {}, print start...".format(args[0])
-    time.sleep(0.1)
 
-def print_name(name):
+def work_func(args, kwargs, callback):
+    print "thread {} start...".format(args[0])
     time.sleep(0.1)
-    print "the name is!!!!!!!!!!!!!!!!!!!!!!!!!! {}".format(name)
+    return args[0]
+
+
+def work_func_cb(task, result):
+    print "nothing, just print the result: {}".format(result)
+
+
 
 if __name__ == '__main__':
     print "start..."
-    zem_thread_pool = MyThreadPool(10, 100)
-    name = 'Grady Wong'
-    zem_thread_pool.add_task(print_name, name)
-    zem_thread_pool.destroy()
+    zem_thread_pool = ThreadPool(10, 100, 100)
+    for i in range(100):
+        zem_thread_pool.add_task(work_func, (i,), callback=work_func_cb)
+    zem_thread_pool.wait()
+    zem_thread_pool.fini_thread_pool()
     print "finish"
-
-
-
-
-
